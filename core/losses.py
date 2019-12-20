7db98857a463d3b3db423382800782beb5dd9757
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2019-12-19 23:40:30 -0200

    Correcting the regularization in the loss function

--- core/losses.py
+++ core/losses.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 
 def mse(output_data_ph, output_data_pred, regularization_penalty=None, weights=None):
 
-    weights_square_sum = sum([tf.nn.l2_loss(weight) for weight in weights])
+    weights_square_sum = sum([tf.reduce_mean(tf.square(weight)) for weight in weights])
 
     return (
-            tf.reduce_mean(tf.square(output_data_ph - output_data_pred)
-            + regularization_penalty*weights_square_sum)
+            tf.reduce_mean(tf.square(output_data_ph - output_data_pred))
+            + regularization_penalty*weights_square_sum
             )
 
 def loss_switcher(case):
