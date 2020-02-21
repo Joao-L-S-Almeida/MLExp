a9901c2b4733c9e3365745fd6d1d31d6ba8bbaf7
Joao Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2020-02-21 23:05:21 +0000

    Assembling the 4D global solution array

--- auxiliary/script_rayleighBernard.py
+++ auxiliary/script_rayleighBernard.py
@@ -26,6 +26,7 @@ def extract_data_from_name(vti_file):
 
     return iteration, partition
 
+
 parser = ArgumentParser(description='Argument parsers')
 parser.add_argument('--path', type=str)
 parser.add_argument('--case', type=str)
@@ -42,6 +43,7 @@ n_variables = len(variables) + 1
 
 number_of_snapshots = len(data_directories)
 number_of_partitions = 7
+default_value = 99999
 
 # Reading the information about the number of points
 hdf5_path = path + 'rayleighBernard.hdf5'
@@ -63,7 +65,7 @@ for ss, vti_file in enumerate(data_directories):
     # Recovering important information from the file name
     iteration, partition = extract_data_from_name(vti_file)
 
-    points_dict[partition] = points.reshape(data_dimensions+(points.shape[-1],))
+    points_dict[partition] = points.reshape(data_dimensions[::-1]+(points.shape[-1],))
 
     print("File {} read".format(vti_file))
 
@@ -84,14 +86,13 @@ for ss, vti_file in enumerate(data_directories):
         if len(array.shape) == 1:
             array = array[:, None]
 
-        array = array.reshape(data_dimensions + (array.shape[-1],))
+        array = array.reshape(data_dimensions[::-1] + (array.shape[-1],))
 
         variables_list.append(array)
         print("Variable {} read".format(name))
 
     variables_array = np.dstack(variables_list)
     solution_dict[partition].append(variables_array)
-
 
     #ds[ss, :, :] = variables_array
 
@@ -127,8 +128,8 @@ x_min = np.array(x_list).min()
 y_max = np.array(y_list).max()
 y_min = np.array(y_list).min()
 
-x_dim = int((x_max - x_min)/spacing[0])+1
-y_dim = int((y_max - y_min)/spacing[1])+1
+x_dim = int((x_max - x_min)/spacing[0]) + 1
+y_dim = int((y_max - y_min)/spacing[1]) + 1
 
 # TODO Now we need to reconstruct the original domain
 # This allocation method should be replaced by a more convenient one
@@ -140,19 +141,45 @@ global_z_array = np.zeros(1)
 global_points_array = np.meshgrid(global_x_array, global_y_array, global_z_array)
 global_points_array = np.dstack(global_points_array)
 
-global_indices_array = ((global_points_array - np.tile(np.array([x_min, y_min, 0]), (y_dim, x_dim, 1)))\
-                        /np.array(spacing)).astype(int)
+global_indices_hor = ((global_x_array - x_min)/spacing[0]).astype(int)
+global_indices_vert = ((global_y_array - y_min)/spacing[1]).astype(int)
+global_indices_deep = np.zeros(1).astype(int)
 
-global_solution_array = np.zeros((number_of_snapshots, y_dim, x_dim, n_variables))
+global_indices_array = np.meshgrid(global_indices_hor, global_indices_vert, global_indices_deep)
+global_indices_array = np.dstack(global_indices_array)
 
+global_solution_array = default_value*np.ones((number_of_snapshots, y_dim, x_dim, n_variables))
 
 for partition in range(number_of_partitions):
 
     points_array = points_dict[partition]
-    local_x_dim, local_y_dim = points_array.shape[:-1]
-    indices_array = ((points_array - np.tile(np.array([x_min, y_min, 0]), (local_x_dim, local_y_dim, 1)))\
-                        /np.array(spacing)).astype(int)
     solution_array = solution_dict[partition]
+
+    x_local_dim, y_local_dim = points_array.shape[:-1]
+
+    x_local_max = points_array[:, :, 0].max()
+    x_local_min = points_array[:, :, 0].min()
+    y_local_max = points_array[:, :, 1].max()
+    y_local_min = points_array[:, :, 1].min()
+
+    x_array = np.linspace(x_local_min, x_local_max, x_local_dim)
+    y_array = np.linspace(y_local_min, y_local_max, y_local_dim)
+    z_array = np.zeros(1)
+
+    indices_hor = ((x_array - x_min) / spacing[0]).astype(int)
+    indices_vert = ((y_array - y_min) / spacing[1]).astype(int)
+    indices_deep = np.zeros(1).astype(int)
+
+    indices_array = np.meshgrid(indices_hor, indices_vert, indices_deep)
+    indices_array = np.dstack(indices_array)
+
+    j_max = indices_array[:, :, 0].max()
+    j_min = indices_array[:, :, 0].min()
+
+    i_max = indices_array[:, :, 1].max()
+    i_min = indices_array[:, :, 1].min()
+
+    global_solution_array[:, i_min:i_max+1, j_min:j_max+1, :] = solution_array
 
     print("Connecting the multiple partitions.")
 
